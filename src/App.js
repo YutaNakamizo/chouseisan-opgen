2884c4f46e45ddece485835220cf89d8c17fd4a7
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2021-03-21 16:13:32 +0900

    material-ui setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core';
+import * as colors from '@material-ui/core/colors';
+import '@fontsource/roboto';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.teal[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      theme={theme}
+    >
+      <div className="App">
+      </div>
+    </ThemeProvider>
   );
 }
 
